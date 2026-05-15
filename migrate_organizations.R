3ad5e1dffb4f23943ef0b52fcab4e1bf82ebce8c
Sean Boots <sean.boots@gmail.com>
2026-05-15 10:54:32 -0700

    Placeholder for organizational migration tool

---
+++ migrate_organizations.R
@@ -0,0 +1,17 @@
+# Load tidyverse libraries, and set up CKAN URLs and API tokens
+source("lib/ckan_helpers.R")
+
+# Migrate all the datasets from one organization to another one
+# https://open.service-uat.yukon.ca/organization/
+
+origin_org <- "tourism-and-culture"
+destination_org <- "economic-development"
+
+# 1. Find all the packages from the origin organization
+
+# 2. Create a function to change the organization for a specific package
+
+# 3. Loop through all the packages and change the organization to the destination organization
+
+# 4. Output a CSV with all the package names that have been changed.
+
