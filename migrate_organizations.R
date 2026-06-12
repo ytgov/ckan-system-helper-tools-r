b3a271225a95ec27cb8af7150d5cec5c672089bc
Katarina Anderson <katarina.anderson@yukon.ca>
2026-06-12 10:41:38 -0700

    Updated Migration script

--- migrate_organizations.R
+++ migrate_organizations.R
@@ -2,16 +2,41 @@
 source("lib/ckan_helpers.R")
 
 # Migrate all the datasets from one organization to another one
-# https://open.service-uat.yukon.ca/organization/
+org_1 = ""
+org_2 =""
+datapack_table = read.csv("resources_by_dataset.csv")
 
-origin_org <- "tourism-and-culture"
-destination_org <- "economic-development"
 
-# 1. Find all the packages from the origin organization
+mass_dataset_move <-function(old_org,new_org,master_sheet) {
+  moved_packages = data.frame(id=c("Package id's in this column"),
+                              Title=c("Title of the package in this column"))
+  file_name= str_c(old_org,"_",new_org,"_migration_log.csv")
+  
+  for (entry in 1:length(master_sheet$id)) {
+    
+    if (master_sheet$organization_name[entry] == old_org){
+      
+      package_owner_org_update(master_sheet$id[entry],organization_id = new_org)
+      
+      #let the system rest between requests
+      
+      Sys.sleep(0.1)
+      
+      package_update_fields <- list(
+        id = master_sheet$id[entry],
+        internal_notes = str_c("Migrated from ",old_org," to ",new_org," on ",now())
+      )
+      
+      package_patch(
+        x = package_update_fields
+      )
+      
+      add_log_entry(str_c("Moved ","'",master_sheet$title[entry],"'"," from ",old_org," to ",new_org))
+      
+      moved_packages[nrow(moved_packages) +1,] = c(master_sheet$id[entry], master_sheet$title[entry]) 
+    }
+  }
+  write.csv(moved_packages,file_name)
+}
 
-# 2. Create a function to change the organization for a specific package
-
-# 3. Loop through all the packages and change the organization to the destination organization
-
-# 4. Output a CSV with all the package names that have been changed.
-
+mass_dataset_move(org_1,org_2,datapack_table)
